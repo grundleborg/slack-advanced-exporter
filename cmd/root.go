801849921460bcf9549f823d948966aa1225523e
George Goldberg <george@gberg.me>
2021-10-19 21:06:04 +0100

    Migrate to using Cobra as the CLI library.

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	inputArchive  string
+	outputArchive string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "slack-advanced-exporter",
+	Short: "The Slack Advanced Exporter ",
+	Long: `The Slack Advanced Exporter is a tool for supplementing official data exports from Slack with the other bits
+and pieces that these don't include.
+
+Version: 0.4.0`,
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&inputArchive, "input-archive", "i", "", "the path to the Slack export archive which you wish to augment")
+	rootCmd.PersistentFlags().StringVarP(&outputArchive, "output-archive", "o", "", "the path to which you would like the output archive to be written")
+	rootCmd.MarkPersistentFlagRequired("input-archive")
+	rootCmd.MarkPersistentFlagRequired("output-archive")
+	rootCmd.AddCommand(fetchAttachmentsCmd)
+	rootCmd.AddCommand(fetchEmailsCmd)
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
--- main.go
+++ main.go
@@ -1,69 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"gopkg.in/urfave/cli.v1"
+	"github.com/grundleborg/slack-advanced-exporter/cmd"
 )
 
 func main() {
-	app := cli.NewApp()
-
-	app.Name = "Slack Advanced Exporter"
-	app.Usage = "A tool to augment official Slack data exports with additional data that Slack does not include by default."
-	app.Version = "0.3.1"
-
-	var inputArchive string
-	var outputArchive string
-	var slackApiToken string
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "input-archive, i",
-			Value:       "",
-			Usage:       "The path to the Slack export archive which you wish to augment.",
-			Destination: &inputArchive,
-		},
-		cli.StringFlag{
-			Name:        "output-archive, o",
-			Value:       "",
-			Usage:       "The path to which you would like the output archive to be written.",
-			Destination: &outputArchive,
-		},
-	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:    "fetch-attachments",
-			Aliases: []string{"a"},
-			Usage:   "Fetch all file attachments and add them to the output archive.",
-			Action: func(c *cli.Context) error {
-				return fetchAttachments(inputArchive, outputArchive, slackApiToken)
-			},
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "api-token",
-					Usage:       "Slack API token. Can be obtained here: https://api.slack.com/docs/oauth-test-tokens",
-					Destination: &slackApiToken,
-				},
-			},
-		},
-		{
-			Name:    "fetch-emails",
-			Aliases: nil,
-			Usage:   "Fetch users' e-mail addresses using Slack API and them to the output archive.",
-			Action: func(c *cli.Context) error {
-				return fetchEmails(inputArchive, outputArchive, slackApiToken)
-			},
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "api-token",
-					Usage:       "Slack API token. Can be obtained here: https://api.slack.com/docs/oauth-test-tokens",
-					Destination: &slackApiToken,
-				},
-			},
-		},
-	}
-
-	app.Run(os.Args)
+	cmd.Execute()
 }
